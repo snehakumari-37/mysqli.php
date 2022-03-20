cda0e15d38960d56714a19aa35bd121dabb9c6bf
snehakumari-37 <92073117+snehakumari-37@users.noreply.github.com>
2022-03-20 15:27:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $hostname = 'sql6.freesqldatabase.com';
 $username ='sql6480104';
-$password = 'sql6480104';
+$password = 'ZyMjJ1CvvH';
 $dbName = 'sql6480104';
 
-$conn = mysqli_connect('sql6.freesqldatabase.com','sql6480104','sql6480104','sql6480104');
+$conn = mysqli_connect('sql6.freesqldatabase.com','sql6480104','ZyMjJ1CvvH','sql6480104');
 if(!$conn){
 die('sorry');
 }
