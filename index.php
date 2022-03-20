539a6e9d8f7fbd0808ec7cf1ecda16d2a6f6f955
snehakumari-37 <92073117+snehakumari-37@users.noreply.github.com>
2022-03-20 15:27:10 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$hostname = 'sql6.freesqldatabase.com';
+$username ='sql6480104';
+$password = 'sql6480104';
+$dbName = 'sql6480104';
+
+$conn = mysqli_connect('sql6.freesqldatabase.com','sql6480104','sql6480104','sql6480104');
+if(!$conn){
+die('sorry');
+}
+else{
+    echo mysqli_error($conn);
+    // echo mysqli_query();
+    echo "wooohoo";
+    // $sql = "INSERT INTO `register` (`username`,`email`,`password`,`gender`) VALUES ('akar bakar','ab@lol.ml','zooon.passwording','m')";
+    // $sql = "INSERT INTO `register` (`username`, `email`, `password`, `gender`) VALUES ('kek', ';dfslkajfdl', 'jlsdfjalf', 'f')";
+    // mysqli_query($conn,$sql);
+    $sql = "DELETE FROM `register` WHERE gender='m'";
+    $result = mysqli_query($conn, $sql);
+    // mysql_query($conn,$sql)
+    
+    if($result)
+    {
+        echo(" happy");
+    }
+    else
+    {
+        echo 'sad';
+    }
+}
+
+
+
+?>
